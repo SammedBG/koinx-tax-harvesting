e6def95760f56a4c9a5eed8a25f5cb6efe4add12
Sammed Ghattad <sammedghattad2132002711@gmail.com>
2026-06-29 22:40:34 +0530

    refactor: update eslint and related plugins in devDependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
@@ -20,12 +21,19 @@ export default [
       globals: globals.browser,
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-vars': 'error',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
